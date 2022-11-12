5165255c2655fc37243daae47ec98183a26c2cfa
Mukul <mkm02790@gmail.com>
2022-11-12 21:14:50 +0530

    Filtration using java stream

--- tci/src/main/java/com/bonus/tci/serviceImpl/BonusCalculatorImpl.java
+++ tci/src/main/java/com/bonus/tci/serviceImpl/BonusCalculatorImpl.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -24,14 +25,24 @@ public class BonusCalculatorImpl implements BonusCalculator{
 	public Map<String, List<Employee>> calculate(List<Bonus> bn) {
 		Date todaysDate =new java.util.Date();
 		List<Bonus> bonusList = new ArrayList<>();
-		for(Bonus bno : bn) {
-			if(todaysDate.after(bno.joiningDate)  && todaysDate.before(bno.exitDate)) {
-				bonusList.add(bno);				
-			}
-		}
-		if(bonusList== null) {
+//Filtration of Employee who have either exit or not yet joined Without Using Java Stream
+ 
+		/*
+		 * for(Bonus bno : bn) 
+		 * {
+		 *  if(todaysDate.after(bno.joiningDate) && todaysDate.before(bno.exitDate))
+		 *   {
+		 *    bonusList.add(bno);
+		 *    }
+		 * }
+		 */
+//With Using Java Stream 
+		bonusList =	bn.stream().filter((a) -> todaysDate.after(a.joiningDate)).filter((a) -> todaysDate.before(a.exitDate)).collect(Collectors.toList());
+		
+		if(bonusList== null) 
+		{
 			System.out.println("Null Obj");
-			}
+		}
 		try {
 		res = employeeMapper.map(bonusList);
 		}
